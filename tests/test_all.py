e3222426ee97ab21904c2f6b866e37c3df7bc8fb
Dani Hodovic <dani.hodovic@gmail.com>
2021-01-07 15:26:23 +0100

    Add deployment of Prometheus exporter

--- tests/test_all.py
+++ tests/test_all.py
@@ -74,6 +74,14 @@ def test_data_replication(target_host):
         assert value == "somevalue"
 
 
+def test_prometheus_metrics(target_host):
+    primary = target_host("primary")
+    url = f'http://{primary.interface("ens5").addresses[0]}:9121/metrics'
+    res = primary.ansible("uri", f"url={url} return_content=true", check=False)
+    assert res["status"] == 200
+    assert "redis_up 1" in res["content"]
+
+
 def _get_master_addr(host):
     output = host.check_output(
         "docker exec sentinel redis-cli -p 26379 SENTINEL get-master-addr-by-name master"
